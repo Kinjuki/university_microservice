0fe16f6cc02358fb6f1c03c7c538736311c66566
Raymondi <raymondimanenwe210@gmail.com>
2023-02-03 19:38:04 +0300

    first commit

---
+++ StudentBilling/src/test/java/com/example/StudentBilling/StudentBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentBilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
