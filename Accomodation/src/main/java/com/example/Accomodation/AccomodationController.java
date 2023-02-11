90e8dea22a280528f2c4eea56ea07d5414adb536
Elieth <>
2023-02-11 11:46:46 +0300

    deleted accountconntroller not relevant

--- Accomodation/src/main/java/com/example/Accomodation/AccomodationController.java
+++ Accomodation/src/main/java/com/example/Accomodation/AccomodationController.java
@@ -1,13 +1,8 @@
 package com.example.Accomodation;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@Controller
-@RequestMapping
-public class AccountController {
+public class AccomodationController {
     @GetMapping("/")
     public String homePage(){
         return "home";
